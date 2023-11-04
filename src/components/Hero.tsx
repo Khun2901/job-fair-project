10a4cc4135fda596d19d2509c77828275450b92e
RuddySparkle <rutthee9999@gmail.com>
2023-11-04 21:16:23 +0700

    add aos

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,9 @@
 'use client'
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
+import 'aos/dist/aos.css';
+import AOS from 'aos';
 
 export default function Hero() {
 
@@ -12,10 +14,15 @@ export default function Hero() {
         ref.current?.scrollIntoView({behavior: 'smooth'});
     };
 
+    useEffect(() => {
+        AOS.init();
+      }, []);
+
     return (
         <div>
             <div className='relative block p-[5px] w-max-screen h-screen'>
-                <Image src={'/banner.png'} alt="background" fill={true} objectFit='cover' priority/>
+                <Image src={'/banner.png'} data-aos='fade-left' data-aos-duration='1000'
+                alt="background" fill={true} objectFit='cover' priority/>
                 <div className='relative top-[25%] z-20 text-left max-w-[51%] px-10 md:px-20'>
                     <h1 className='text-md md:text-xl xl:text-4xl font-bold mb-2'>Intanuay Job Fair</h1>
                     <h2 className='text-sm md:text-lg xl:text-2xl font-semibold mb-4'>"Connecting Talent, Fueling Dreams"</h2>
@@ -42,7 +49,8 @@ export default function Hero() {
 
             <div ref={ref} className='relative block w-max-screen h-screen' 
             >
-                <Image src={'/scroll1.png'} alt="background" fill={true} objectFit='cover' priority/>
+                <Image src={'/scroll1.png'} data-aos='fade-right' data-aos-duration='1000'
+                alt="background" fill={true} objectFit='cover' priority/>
             </div>
 
         </div>
