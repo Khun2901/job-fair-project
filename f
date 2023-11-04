5534441e6e58422b9c8eba6a7f72c1b2aa0d9280
Khun2901 <nunnun.234@gmail.com>
2023-11-04 17:59:07 +0700

    fix bugs T-T

---
+++ f
@@ -0,0 +1,86 @@
+[33mcommit 64b63e4e52b4daeb02eefed18e4111c58fb98d6c[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Merge: 41a6de6 8cd4c00
+Author: Khun2901 <nunnun.234@gmail.com>
+Date:   Sat Nov 4 16:48:22 2023 +0700
+
+    Merge branch 'main' of https://github.com/Khun2901/job-fair-project
+
+[33mcommit 8cd4c002ef57c238474e611f7c43db44df5bbaef[m
+Author: Khun2901 <nunnun.234@gmail.com>
+Date:   Sat Nov 4 16:41:53 2023 +0700
+
+    add companyDetailPage, add top margin 75px and change bg color in global.css
+
+[33mcommit 63107eb88046066e28eafd610279d4c5e0e9bf93[m
+Author: Khun2901 <nunnun.234@gmail.com>
+Date:   Sat Nov 4 16:01:01 2023 +0700
+
+    add companyCatalog fetched from mongodb
+
+[33mcommit 41a6de64d70ae51aba36c9ae54ade9ec603e89f4[m
+Author: Khun2901 <nunnun.234@gmail.com>
+Date:   Fri Nov 3 18:53:43 2023 +0700
+
+    Revert "update Next.js version, add actions"
+    
+    This reverts commit 573cb0261e6ca8229cde772fe7ecd6cfb4d343bb.
+
+[33mcommit 573cb0261e6ca8229cde772fe7ecd6cfb4d343bb[m
+Author: RuddySparkle <rutthee9999@gmail.com>
+Date:   Fri Nov 3 02:16:08 2023 +0700
+
+    update Next.js version, add actions
+
+[33mcommit 035957e9c6b9d86621819e0cdf05130ae7cc8778[m
+Author: Rutthee Raywatkhunanon <rutthee9999@gmail.com>
+Date:   Sun Oct 29 15:11:27 2023 +0700
+
+    install redux, add hooks, add redux, interview page & cart, edit some components
+
+[33mcommit 5b5b67682f5de653171aaf9c070fdcdca9511edc[m[33m ([m[1;32mkhun[m[33m)[m
+Author: Rutthee Raywatkhunanon <rutthee9999@gmail.com>
+Date:   Tue Oct 24 01:08:04 2023 +0700
+
+    add some action and companycards in company page
+
+[33mcommit c0e270ae1b65f212741115b9a8e5d15e4ec33601[m
+Author: Rutthee Raywatkhunanon <rutthee9999@gmail.com>
+Date:   Mon Oct 23 19:22:04 2023 +0700
+
+    edit layout and init interview page
+
+[33mcommit 4dd1ae46350986b1d519e52e74291faef192c765[m
+Author: Khun2901 <nunnun.234@gmail.com>
+Date:   Sun Oct 22 21:36:02 2023 +0700
+
+    global.css edited
+
+[33mcommit a46ed8fb681c2a0074e586c8e25c58e8f666e2d2[m
+Author: Rutthee Raywatkhunanon <rutthee9999@gmail.com>
+Date:   Sat Oct 21 22:25:34 2023 +0700
+
+    add hero, edit navbar
+
+[33mcommit f08a66a879d385ca1998f81c5be720c73cb26d8c[m
+Author: Rutthee Raywatkhunanon <rutthee9999@gmail.com>
+Date:   Fri Oct 20 00:03:27 2023 +0700
+
+    install frameworks
+
+[33mcommit 35575e781a1304f49bdfd05ce57a6f3d09d723ee[m
+Author: Rutthee Raywatkhunanon <rutthee9999@gmail.com>
+Date:   Sun Oct 8 22:46:45 2023 +0700
+
+    init some components
+
+[33mcommit 3412d4979ad42b7906eded93c85459efa5b59676[m
+Author: Rutthee Raywatkhunanon <rutthee9999@gmail.com>
+Date:   Sun Oct 8 21:59:51 2023 +0700
+
+    just clone
+
+[33mcommit a39d1ff3c4cb82e72423ec0571ed04449cc2f5c9[m
+Author: Khun2901 <nunnun.234@gmail.com>
+Date:   Sun Oct 8 20:52:55 2023 +0700
+
+    Initial commit from Create Next App
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,38 +1,50 @@
 'use client'
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { useRef } from 'react';
 
 export default function Hero() {
 
     const router = useRouter();
+    const ref = useRef<HTMLDivElement>(null);
+
+    const handleClick = () => {
+        ref.current?.scrollIntoView({behavior: 'smooth'});
+    };
 
     return (
-        <div className='relative block m-0 p-[5px] w-screen h-screen' >
-            <Image src={'/banner.png'} alt="background" fill={true} objectFit='cover' priority/>
-            <div className='relative top-[25%] z-20 text-left max-w-[51%] px-20'>
-                <h1 className='text-4xl font-bold mb-2'>Intanuay Job Fair</h1>
-                <h2 className='text-2xl font-semibold mb-4'>"Connecting Talent, Fueling Dreams"</h2>
-                <h3 className='text-xl font-serif text-justify'>Our online Intanuay Job Fair is a platform 
-                for you to take control of your future, gain practical experience, and learn from industry experts. 
-                Whether you're a student seeking internships, a fresh graduate looking for your first job, or an 
-                experienced professional ready for a change, we're here to guide you on your beautiful journey.</h3>
-                <div className='flex justify-evenly'>
-                    <button className='w-[200px] bg-[#111b47] text-white border-[#111b47]
-                        font-semibold text-[18px] py-1 px-4 rounded-lg mt-10 border-2 
-                        hover:bg-white hover:text-[#111b47] hover:border-[#111b47]'
-                        onClick={(e)=>{e.stopPropagation(); router.push('/company')}}>
-                        Browse Company
-                    </button>
-                    <button className='w-[200px] text-[#111b47] bg-white border-[#111b47]
-                        font-semibold text-[18px] py-1 px-4 rounded-lg mt-10 border-2 
-                        hover:bg-[#111b47] hover:text-white'
-                        onClick={(e)=>{e.stopPropagation(); router.push('/interview')}}>
-                        Interview Session
-                    </button>
+        <div>
+            <div className='relative block p-[5px] w-max-screen h-screen'>
+                <Image src={'/banner.png'} alt="background" fill={true} objectFit='cover' priority/>
+                <div className='relative top-[25%] z-20 text-left max-w-[51%] px-10 md:px-20'>
+                    <h1 className='text-md md:text-xl xl:text-4xl font-bold mb-2'>Intanuay Job Fair</h1>
+                    <h2 className='text-sm md:text-lg xl:text-2xl font-semibold mb-4'>"Connecting Talent, Fueling Dreams"</h2>
+                    <h3 className='text-sm md:text-md xl:text-lg font-serif text-justify'>A platform 
+                    for you to take control of your future, gain practical experience, and learn from industry experts. 
+                    Whether you're a student seeking internships, a fresh graduate looking for your first job, or a 
+                    professional ready for a change, we're here to guide you on your beautiful journey.</h3>
+                    <div className='flex justify-evenly gap-4'>
+                        <button className='w-auto bg-[#111b47] text-white border-[#111b47]
+                            font-semibold text-[18px] py-1 px-10 rounded-lg mt-10 border-2 
+                            hover:bg-white hover:text-[#111b47] hover:border-[#111b47]'
+                            onClick={(e)=>{e.stopPropagation(); router.push('/company')}}>
+                            Browse Company
+                        </button>
+                        <button className='w-auto text-[#111b47] bg-white border-[#111b47]
+                            font-semibold text-[18px] py-1 px-10 rounded-lg mt-10 border-2 
+                            hover:bg-[#111b47] hover:text-white'
+                            onClick={handleClick}>
+                            Explore Our Campaign
+                        </button>
+                    </div>
                 </div>
             </div>
 
-            
+            <div ref={ref} className='relative block w-max-screen h-screen' 
+            >
+                <Image src={'/scroll1.png'} alt="background" fill={true} objectFit='cover' priority/>
+            </div>
+
         </div>
     );
 }
